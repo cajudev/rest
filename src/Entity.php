dbb746539d7eed14c83ead4bd214c994c0cd896f
Richard Lopes <richard.cirux@gmail.com>
2020-04-07 23:20:35 -0300

    refactor(entity): change access methods

--- src/Entity.php
+++ src/Entity.php
@@ -2,27 +2,39 @@
 
 namespace Cajudev\RestfulApi;
 
+use Doctrine\Common\Collections\Collection;
+
 abstract class Entity
 {
     public function __construct(array $properties = [])
     {
-        $this->setParams($properties);
+        $this->populate($properties);
     }
 
-    public function setParams(array $properties = [])
+    public function populate(array $properties = [])
     {
         foreach ($properties as $property => $value) {
-            $this->set($property, $value);
+            $this->$property = $value;
         }
     }
-    
-    public function set(string $property, $value)
+
+    public function __get($property)
     {
-        if ($value !== null) {
-            $method = 'set' . ucfirst($property);
-            if (method_exists($this, $method)) {
-                $this->$method($value);
-            }
+        $ref = new \ReflectionProperty($this, strtolower($property));
+        $ref->setAccessible(true);
+        $value = $ref->getValue($this);
+
+        if ($value instanceof Collection) {
+            return new CollectionProxy($this, $value);
         }
+
+        return $value;
+    }
+
+    public function __set($property, $value)
+    {
+        $ref = new \ReflectionProperty($this, strtolower($property));
+        $ref->setAccessible(true);
+        $ref->setValue($this, $value);
     }
 }
