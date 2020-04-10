51c8f15ab4bc0f22d2a6080f519df9054449d612
Richard Lopes <richard.cirux@gmail.com>
2020-04-09 22:32:54 -0300

    feat(entity): add payload method

--- src/Entity.php
+++ src/Entity.php
@@ -1,9 +1,12 @@
 <?php
 
-namespace Cajudev\RestfulApi;
+namespace Cajudev\Rest;
 
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Annotations\AnnotationReader;
+
+use Cajudev\Rest\Annotation\Payload;
 
 /**
  * @ORM\MappedSuperclass
@@ -11,19 +14,33 @@ use Doctrine\Common\Collections\Collection;
  */
 abstract class Entity
 {
-    /** @ORM\Id @ORM\Column(type="integer") @ORM\GeneratedValue **/
+    /**
+     * @Payload
+     * 
+     * @ORM\Id @ORM\Column(type="integer") @ORM\GeneratedValue
+     */
     protected int $id = 0;
 
-    /** @ORM\Column(type="boolean", nullable=false) */
+    /**
+     * @Payload
+     * 
+     * @ORM\Column(type="boolean", nullable=false)
+     */
     protected bool $active = true;
 
-    /** @ORM\Column(type="boolean", nullable=false) */
+    /**
+     * @ORM\Column(type="boolean", nullable=false)
+     */
     protected bool $excluded = false;
 
-    /** @ORM\Column(type="datetime", nullable=false) */
+    /**
+     * @ORM\Column(type="datetime", nullable=false)
+     */
     protected \DateTime $createdAt;
 
-    /** @ORM\Column(type="datetime", nullable=false) */
+    /**
+     * @ORM\Column(type="datetime", nullable=false)
+     */
     protected \DateTime $updatedAt;
 
     public function __construct($properties = [])
@@ -53,6 +70,66 @@ abstract class Entity
     public function preUpdate(): void
     {
         $this->updatedAt = new \DateTime();
+    }
+
+    /**
+     * Return the payload representation of the entity
+     *
+     * @return object
+     */
+    public function payload(): object {
+        $reader  = new AnnotationReader();
+        $payload = new \StdClass();
+
+        foreach ($this->getReflection()->getProperties() as $property) {
+            if ($annotation = $reader->getPropertyAnnotation($property, Payload::class)) {
+                $payload->{$property->getName()} = $this->addPayloadByAnnotation($annotation, $property);
+            }
+        }
+        
+        return $payload;
+    }
+
+    private function addPayloadByAnnotation(Payload $annotation, \ReflectionProperty $property) {
+        $property->setAccessible(true);
+
+        if ($property->getValue($this) instanceof Entity) {
+            return $this->addPayloadByAnnotationUsingEntity($annotation, $property->getValue($this));
+        }
+        
+        if ($property->getValue($this) instanceof Collection) {
+            return $this->addPayloadByAnnotationUsingCollection($annotation, $property->getValue($this));
+        }
+
+        return $property->getValue($this);
+    }
+
+    private function addPayloadByAnnotationUsingEntity(Payload $annotation, Entity $entity) {
+        if ($annotation->property) {
+            return $entity->{$annotation->property};
+        }
+
+        if ($annotation->properties) {
+            $return = new \StdClass();
+
+            foreach ($annotation->properties as $property) {
+                $return->$property = $entity->$property;
+            }
+
+            return $return;
+        }
+
+        return $entity->payload();
+    }
+
+    private function addPayloadByAnnotationUsingCollection(Payload $annotation, Collection $entities) {
+        $return = [];
+
+        foreach ($entities as $key => $entity) {
+            $return[$key] = $this->addPayloadByAnnotationUsingEntity($annotation, $entity);
+        }
+
+        return $return;
     }
 
     public function __get($property)
