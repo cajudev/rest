8e4209e93d4bc33e6d09724ecde266a53daa8368
Richard Lopes <richard.cirux@gmail.com>
2020-04-10 18:57:45 -0300

    refactor(validations): update string errors message

--- src/Annotations/Validations/Strings.php
+++ src/Annotations/Validations/Strings.php
@@ -32,15 +32,21 @@ final class Strings extends AbstractAnnotationValidator
         }
 
         if ($this->length !== null && strlen($string) !== $this->length) {
-            throw new BadRequestException("Parâmetro [$property] deve possuir exatamente {$this->length} caracteres");
+            throw new BadRequestException(
+                sprintf("Parâmetro [%s] deve possuir exatamente %d %s.", $property, $this->length, $this->length > 1 ? 'caracteres' : 'caractere')
+            );
         }
 
         if ($this->maxlength !== null && strlen($string) > $this->maxlength) {
-            throw new BadRequestException("Parâmetro [$property] deve possuir no máximo {$this->maxlength} caracteres");
+            throw new BadRequestException(
+                sprintf("Parâmetro [%s] deve possuir no máximo %d %s.", $property, $this->maxlength, $this->maxlength > 1 ? 'caracteres' : 'caractere')
+            );
         }
 
         if ($this->minlength !== null && strlen($string) < $this->minlength) {
-            throw new BadRequestException("Parâmetro [$property] deve possuir no mínimo {$this->minlength} caracteres");
+            throw new BadRequestException(
+                sprintf("Parâmetro [%s] deve possuir no mínimo %d %s.", $property, $this->minlength, $this->minlength > 1 ? 'caracteres' : 'caractere')
+            );
         }
 
         return $string;
