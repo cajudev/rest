1ac27200ef1f263b0da85e23f4e68b903d74f427
Richard Lopes <richard.cirux@gmail.com>
2020-04-11 23:03:56 -0300

    feat(repository): override find methods

--- src/Repository.php
+++ src/Repository.php
@@ -9,11 +9,32 @@ use Cajudev\Rest\Collections\EntityCollection;
 
 abstract class Repository extends EntityRepository
 {
+    /**
+     * @Override
+     */
+    public function find($id, $lockMode = NULL, $lockVersion = NULL)
+    {
+        return $this->findOneBy(['id' => $id]);
+    }
+
+    /**
+     * @Override
+     */
+    public function findOneBy(array $criteria, array $orderBy = null) {
+        return parent::findOneBy($criteria + ['excluded' => false], $orderBy);
+    }
+
+    /**
+     * @Override
+     */
     public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
     {
         return new EntityCollection(parent::findBy($criteria, $orderBy, $limit, $offset));
     }
 
+    /**
+     * @Override
+     */
     public function matching(Criteria $criteria)
     {
         return new EntityCollection(parent::matching($criteria));
